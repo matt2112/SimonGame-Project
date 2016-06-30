45aaf6fd72010a40a46b4140743983151dd35ae6
matt2112 <matt_lewis@live.co.uk>
2016-06-30 21:16:00 +0100

    change colours and add sounds

--- scripts.js
+++ scripts.js
@@ -1,20 +1,55 @@
-var sequence = ["red", "green", "blue", "yellow"];
+var sequence = ["red", "green", "blue", "yellow", "blue", "green"];
+var counter = 0;
+var timer;
+var running = false;
 
 var colors = {
+    "green": {
+        "normal": "#008000",
+        "change": "#00e600"
+    },
     "red": {
-        "normal": "red",
-        "change": "black"
+        "normal": "#991f00",
+        "change": "#ff471a"
+    },
+    "yellow": {
+        "normal": "#cccc00",
+        "change": "#ffff00"
+    },
+    "blue": {
+        "normal": "#000066",
+        "change": "#1a1aff"
     }
-}
+};
 
 $("#start").on("click", function() {
     
-    for (var i = 0; i < sequence.length; i++) {
-        var newColor = colors[sequence[i]]["change"];
-        var oldColor = colors[sequence[i]]["normal"];
-        $("#" + sequence[i]).css("background-color", newColor);
-        setTimeout(function() {
-            $("#" + sequence[i]).css("background-color", oldColor);
-        }, 2000);
-    } 
+    if (!running) {
+        counter = 0;
+        running = true;
+        timer = window.setInterval($traverseSequence, 1000);
+    }
+    
 });
+
+$traverseSequence = function() {
+    if (counter > 0) {
+        var oldColor = colors[sequence[counter - 1]]["normal"];
+        $("#" + sequence[counter - 1]).css("background-color", oldColor);
+    }
+    
+    if (counter < sequence.length) {
+        var newColor = colors[sequence[counter]]["change"];
+        $("#" + sequence[counter]).css("background-color", newColor);
+        $("#" + sequence[counter] + "sound")[0].play();
+        counter += 1;
+    } else {
+        window.clearInterval(timer);
+        running = false;
+    }   
+}
+
+/**
+Sounds to use later:
+https://s3.amazonaws.com/freecodecamp/simonSound1.mp3, https://s3.amazonaws.com/freecodecamp/simonSound2.mp3, https://s3.amazonaws.com/freecodecamp/simonSound3.mp3, https://s3.amazonaws.com/freecodecamp/simonSound4.mp3.
+*/
